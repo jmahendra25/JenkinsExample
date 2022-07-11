fcdaad40587196607baf6b30fb4cf5ccdc21dd23
mahendra <jmahendra25@gmail.com>
2022-07-11 17:10:52 +0530

    push all spring boot application code

---
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+	
+	
+	@GetMapping("/")
+	public String home() {
+		return "Welcome to Jenkins Project";
+	}
+
+}
